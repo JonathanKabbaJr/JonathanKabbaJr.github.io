e92e0e0356eba8a545be62e8657b386b109c2c1e
JonathanKabbaJr <jkabba@umd.edu>
2020-11-02 23:29:18 -0500

    Lab_7
    changes made

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,10 +1,15 @@
 // Get saved data from sessionStorage
-let name = ""; // Your code here
-let semester =  ""; // Your code here
-let credits =  ""; // Your code here
-let description =  ""; // Your code here 
+let name = sessionStorage.getItem('name'); // Your code here
+let semester =  sessionStorage.getItem('semester'); // Your code here
+let credits =  sessionStorage.getItem('credits'); // Your code here
+let description =  sessionStorage.getItem('description'); // Your code here 
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
-
+if (name && semester) {
+    document.getElementById('name').innerText = name;
+    document.getElementById('semester').innerText = semester;
+    document.getElementById('credits').innerText = credits;
+    document.getElementById('description').innerText = description;
+}
 
--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,17 +1,51 @@
 const main = document.getElementById("main");
 
-function getClasses(){
-    
-    let classId =  ""; // Your code here
+async function getClasses() {
+    debugger;
+    let classId = document.getElementById('courseId').value; // Your code here
 
-    if(typeof classId !== "undefined" & classId !== ""){
-        
-        let classURL =  ""; // Your code here
+    if (typeof classId !== "undefined" & classId !== "") {
+
+        let classURL = "https://api.umd.io/v0/courses/" + classId; // Your code here
 
         // YOUR CODE HERE
-        
+        const resRaw = await fetch(classURL);
+        const response = await resRaw.json();
+        console.log(response);
+        const courseObject = {
+            course_id: "ENEE380",
+            name: "Electromagnetic Theory",
+            dept_id: "ENEE",
+            department: "Electrical & Computer Engineering",
+            semester: "201501",
+            credits: "3",
+            grading_method: ["Regular"],
+            core: [],
+            gen_ed: [],
+            description: "Introduction to electromagnetic...",
+            relationships: {
+                coreqs: [],
+                prereqs: ["Prerequisite: PHYS271, PHYS270, ..."],
+                restrictions: [],
+                restricted_to: [],
+                credit_only_granted_for: [],
+                credit_granted_for: [],
+                formerly: [],
+                also_offered_as: []
+            },
+            sections: [
+              "ENEE380-0101",
+              "ENEE380-0102",
+              "ENEE380-0103"
+            ]
+        };
+        // Store in session storage
+        sessionStorage.setItem('name', courseObject.name);
+        sessionStorage.setItem('semester', courseObject.semester);
+        sessionStorage.setItem('credits', courseObject.credits);
+        sessionStorage.setItem('description', courseObject.description);
     }
-    else{
+    else {
         main.innerHTML = "No value provided";
     }
 }
