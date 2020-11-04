bd3571a72e0b30a1c954b7111624b81a9e47f609
JonathanKabbaJr <jkabba@umd.edu>
2020-11-04 12:07:44 -0500

    Lab_7
    Lab 7 code addition

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,14 @@
 // Get saved data from sessionStorage
-let title = ""; // Your code here
-let lat_max = "";
-let lat_min = "";
-let lon_max = "";
-let lon_min = "";
+let title = sessionStorage.getItem('title'); // Your code here
+let lat_max = sessionStorage.getItem('latMax');
+let lat_min = sessionStorage.getItem('latMin');
+let lon_max = sessionStorage.getItem('lonMax');
+let lon_min = sessionStorage.getItem('lonMin');
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+document.getElementById('title').innerText = title || 'Nothing set';
+document.getElementById('lat_max').innerText = lat_max || 'Nothing set';
+document.getElementById('lat_min').innerText = lat_min || 'Nothing set';
+document.getElementById('lon_max').innerText = lon_max || 'Nothing set';
+document.getElementById('lon_min').innerText = lon_min || 'Nothing set';
--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,22 +1,48 @@
 const main = document.getElementById("main");
 
-function getBusRoute() {
-  let busRoute = ""; // Your code here
+async function getBusRoute() {
+  let busRoute = document.getElementById("busroute").value;
 
   if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
-    let busRouteURL = ""; // Your code here
+    let busRouteURL = "https://api.umd.io/v0/bus/routes/" + busRoute;
 
-    fetch(busRouteURL)
-      .then((response) => {
-        return response.json();
-      })
-      .then((route) => {
-        // YOUR CODE HERE
-      })
-      .catch((err) => {
+    try {
+       
+        const resRaw = await fetch(busRouteURL);
+        const route = await resRaw.json();
+        let title, latMax, latMin, lonMax, lonMin;
+        let message = "";
+        if (typeof route.title !== "undefined") {
+          title = route.title;
+          latMax = route.lat_max;
+          latMin = route.lat_min;
+          lonMax = route.lon_max;
+          lonMin = route.lon_min;
+          sessionStorage.setItem('title', title);
+          sessionStorage.setItem('latMax', latMax);
+          sessionStorage.setItem('latMin', latMin);
+          sessionStorage.setItem('lonMax', lonMax);
+          sessionStorage.setItem('lonMin', lonMin);
+          message =
+            title +
+            " <br>Lat max : " +
+            latMax +
+            " <br>Lat min : " +
+            latMin +
+            " <br>Lon Max : " +
+            lonMax +
+            " <br>Lon Min :" +
+            lonMin;
+        } else {
+          message = "No bus info available";
+        }
+        console.log(title);
+        main.innerHTML = "Bus Route Info: " + message;
+    } catch (err) {
         console.log(err);
         main.innerHTML = "Invalid bus route";
-      });
+    }
+      
   } else {
     main.innerHTML = "No value provided";
   }
