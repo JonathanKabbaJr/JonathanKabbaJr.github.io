c57cce39a346e8816c967949a1c8005d5440ce5c
JonathanKabbaJr <jkabba@umd.edu>
2020-11-03 22:47:27 -0500

    Lab_7 update
    Lab 7 update

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,15 +1,9 @@
 // Get saved data from sessionStorage
-let name = sessionStorage.getItem('name'); // Your code here
-let semester =  sessionStorage.getItem('semester'); // Your code here
-let credits =  sessionStorage.getItem('credits'); // Your code here
-let description =  sessionStorage.getItem('description'); // Your code here 
+let title = ""; // Your code here
+let lat_max = "";
+let lat_min = "";
+let lon_max = "";
+let lon_min = "";
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
-if (name && semester) {
-    document.getElementById('name').innerText = name;
-    document.getElementById('semester').innerText = semester;
-    document.getElementById('credits').innerText = credits;
-    document.getElementById('description').innerText = description;
-}
-
--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,51 +1,23 @@
 const main = document.getElementById("main");
 
-async function getClasses() {
-    debugger;
-    let classId = document.getElementById('courseId').value; // Your code here
+function getBusRoute() {
+  let busRoute = ""; // Your code here
 
-    if (typeof classId !== "undefined" & classId !== "") {
+  if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
+    let busRouteURL = ""; // Your code here
 
-        let classURL = "https://api.umd.io/v0/courses/" + classId; // Your code here
-
+    fetch(busRouteURL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((route) => {
         // YOUR CODE HERE
-        const resRaw = await fetch(classURL);
-        const response = await resRaw.json();
-        console.log(response);
-        const courseObject = {
-            course_id: "ENEE380",
-            name: "Electromagnetic Theory",
-            dept_id: "ENEE",
-            department: "Electrical & Computer Engineering",
-            semester: "201501",
-            credits: "3",
-            grading_method: ["Regular"],
-            core: [],
-            gen_ed: [],
-            description: "Introduction to electromagnetic...",
-            relationships: {
-                coreqs: [],
-                prereqs: ["Prerequisite: PHYS271, PHYS270, ..."],
-                restrictions: [],
-                restricted_to: [],
-                credit_only_granted_for: [],
-                credit_granted_for: [],
-                formerly: [],
-                also_offered_as: []
-            },
-            sections: [
-              "ENEE380-0101",
-              "ENEE380-0102",
-              "ENEE380-0103"
-            ]
-        };
-        // Store in session storage
-        sessionStorage.setItem('name', courseObject.name);
-        sessionStorage.setItem('semester', courseObject.semester);
-        sessionStorage.setItem('credits', courseObject.credits);
-        sessionStorage.setItem('description', courseObject.description);
-    }
-    else {
-        main.innerHTML = "No value provided";
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+        main.innerHTML = "Invalid bus route";
+      });
+  } else {
+    main.innerHTML = "No value provided";
+  }
 }
