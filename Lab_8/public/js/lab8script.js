df562577786961af2d96c9616f14647c71a0ed4a
JonathanKabbaJr <jkabba@umd.edu>
2020-11-16 06:33:22 -0500

    Lab_8

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -2,36 +2,35 @@
 let url = "https://www.reddit.com/r/popular.json";
 let chart = "";
 console.log(url);
-//const fetchPromise = fetch(url);
 
 let settings = { method: "Get" };
 let chartValues = [];
 
 async function getData() {
-    await fetch(url, settings)
-        .then(res => res.json())
-        .then((json) => {
-            let listSize = json.data.children.length;
-            // Loop to pick 5 random entries
-            for (x = 0; x < 5; x++) {
-                /*
-                    Get a random number within the size of the list
-                    Get subreddit, author, title, and ups from record
-                    Set the message to be:
-                        let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
-                    Add a new <li> element with the message to the 'redditList' element
-                    Add a data entry to chartValues with author as the label and ups as the y component
-                */
-    
-
-                /*.......*/
-                
-                let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
-                /*.......*/
-            }
-        })
-        .then(values => console.log(chartValues));
-        //chart.render(); // Do you need to remove the comments from here in order to get it to work?
+    const resRaw = await fetch(url, settings);
+    const json = await resRaw.json();
+    let listSize = json.data.children.length;
+    // Loop to pick 5 random entries
+    for (x = 0; x < 5; x++) {
+        debugger;
+        // Get a random number within the size of the list
+        const rand = Math.floor((Math.random() * (listSize-1)));
+        // Get subreddit, author, title, and ups from record
+        const post = json.data.children[rand].data;
+        const subreddit = post.subreddit;
+        const author = post.author;
+        const title = post.title;
+        const ups = post.ups;
+        // Set the message to be:
+        let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
+        //  Add a new <li> element with the message to the 'redditList' element
+        const ul = document.getElementById('redditList');
+        ul.innerHTML += `<li>${message}</li>`;
+        // Add a data entry to chartValues with author as the label and ups as the y component
+        let addToChart = { label: author, y: ups };
+        chartValues.push(addToChart);
+    }
+    chart.render();
 };
 
 window.onload = async function makeChart() {
@@ -45,12 +44,10 @@ window.onload = async function makeChart() {
             { 
                 type: "column",
                 name: "Popular Reddit",
-                dataPoints: // WHAT GOES HERE???
+                dataPoints: chartValues
             }
         ]
     });
     
     chart.render();
 }
-
-//window.onload = makeChart();
--- Lab_8/server.js
+++ Lab_8/server.js
@@ -1,6 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
 // Code here for how to set 'public' as the static folder for express
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Code here to have the app listen on port 8080....please provide a console.log message
+const PORT = 8080;
+app.listen(PORT, function(){
+    console.log('Server listening on port: ' + PORT);
+});
